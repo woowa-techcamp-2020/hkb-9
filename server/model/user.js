ce1061bd30ddf85ad1e713a6786c10ce23f05315
jungcome7 <jungcome7@gmail.com>
2020-07-31 02:58:33 +0900

    feat: fix createUser module

--- server/model/user.js
+++ server/model/user.js
@@ -19,7 +19,7 @@ exports.findOne = async (connection, loginId, password) => {
 
 exports.createUser = async (connection, option) => {
   const { loginId, passwordHash: { password, salt } } = option;
-  const [insertId] = await connection.query(
+  const [{ insertId }] = await connection.query(
     `INSERT INTO user(login_id, password, salt, refresh_token) VALUES('${loginId}', '${password}', '${salt}', 'temp')`,
   );
   return insertId;
