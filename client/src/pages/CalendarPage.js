175a4d660962ecd18deb578f8420358c765568a3
jungcome7 <jungcome7@gmail.com>
2020-08-03 00:56:30 +0900

    [#30] feat - 메인, 캘린더 페이지 폴더 삭제

--- client/src/pages/CalendarPage.js
+++
@@ -1,9 +0,0 @@
-import { Calendar } from '../components/Calendar';
-
-export default class CalendarPage {
-  constructor() {
-    this.$target = document.querySelector('main');
-
-    new Calendar();
-  }
-}
