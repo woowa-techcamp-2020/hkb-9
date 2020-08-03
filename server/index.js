13d0d9d51c147c6644cdc9b2714d25be7dea3079
Donguk Hong <mbxd1@naver.com>
2020-08-03 11:23:46 +0900

    [#29] feat - server에서 서빙

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+const appRoot = require('app-root-path');
 const passport = require('passport');
 const { urlencoded, json } = require('express');
 const apiRoutes = require('./routes');
@@ -11,12 +13,17 @@ const passportConfig = require('./lib/passport');
 const app = express();
 
 app.use(urlencoded({ extended: true }), json());
+app.use(express.static(appRoot.resolve('client/dist')));
 app.use(passport.initialize());
 passportConfig();
 
 app.use(morgan('dev'));
 
 app.use('/api', apiRoutes);
+app.use((req, res) => {
+  res.sendFile(appRoot.resolve('client/dist/index.html'));
+});
+
 app.use(errorHandler);
 
 app.listen(SERVER_PORT, () => {
