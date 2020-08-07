cab56ed97015e53b2792aa7ba7e443206ad585c9
jungcome7 <jungcome7@gmail.com>
2020-08-07 11:10:12 +0900

    [#30] feat - 내역페이지 디자인 수정

--- client/src/components/Amount/template.js
+++ client/src/components/Amount/template.js
@@ -1,16 +1,16 @@
-import { html } from '../../utils/functions';
+import { html, printNumberWithCommas } from '../../utils/functions';
 
 const amountTemplate = (income, expense) => html`
   <form class="amount-form">
     <div class="income">
       <input type="checkbox" id="checkbox-income" name="type" />
       <label class="income-label" for="checkbox-income">수입</label>
-      <span class="total-income">${income}원</span>
+      <span class="wrap"><span class="total-income">${printNumberWithCommas(income)}원</span></span>
     </div>
     <div class="expense">
       <input type="checkbox" id="checkbox-expense" name="type" />
       <label class="expense-label" for="checkbox-expense">지출</label>
-      <span class="total-expense">${expense}원</span>
+      <span class="wrap"><span class="total-expense">${printNumberWithCommas(expense)}원</span></span>
     </div>
   </form>
 `;
--- client/src/components/Main/Item/template.js
+++ client/src/components/Main/Item/template.js
@@ -8,7 +8,7 @@ export const itemTemplate = ({ month, date, income, expense, day }) => html`
   <div class="item-header">
     <span class="date">${month}월 ${date}일</span>
     <span class="day">${day}</span>
-    <span class="date-income">+${printNumberWithCommas(income)}원</span>
+    <span class="date-income">${printNumberWithCommas(income)}원</span>
     <span class="date-expense">-${printNumberWithCommas(expense)}원</span>
   </div>
   <div class="item-list" id="item-list-${date}"></div>
